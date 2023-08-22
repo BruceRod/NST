a77cd840273db13f1d5ab6de17ffaf27d98eb03f
BruceRod13 <brucerodrigues13@icloud.com>
2023-08-22 12:34:34 +0530

    day 1

---
+++ index.js
@@ -0,0 +1,33 @@
+function firstFunction (input){
+    console.log(input)
+}
+
+firstFunction("function")
+function secondFunction (){
+    console.log(1)
+}
+secondFunction()
+firstFunction()
+
+function double (num)
+{
+    console.log(num * 2)
+}
+double(5)
+
+function tripple (num)
+{
+console.log(num * 3)
+}
+tripple(10)
+
+function add (num1, num2)
+{
+    console.log(num1 + num2) 
+}
+add(2,6)
+function multiply (num1, num2)
+{
+    console.log(num1 * num2)
+}
+multiply(4,2)
